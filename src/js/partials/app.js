e941ff65c77577556869bb7b8d75e382404c2443
Данила Лужин <d.m.luzhin@gmail.com>
2017-04-14 15:32:13 +0300

    update

---
+++ src/js/partials/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$(document).on('click', '.history', function(e) {
+		e.preventDefault();
+
+		var href = $(this).attr('href');
+		getContent(href, true);
+	})
+
+
+});
+
+window.addEventListener("popstate", function(e){
+
+	getContent(location.pathname, false);
+
+});
+
+/*var doc = document;*/
+
+function getContent(url, addEntry) {
+	$.get(url).done(function(data) {
+
+		$('#content').html($(data).find('#content').html());
+		if(addEntry == true) {
+
+			history.pushState(null, null, url);
+		}
+
+	});
+}
