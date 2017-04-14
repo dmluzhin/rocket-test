aba1006827af17a5f9f8ff31aba3b45981f18c6f
Данила Лужин <d.m.luzhin@gmail.com>
2017-04-14 17:41:42 +0300

    upd

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -27,3 +27,9 @@ function getContent(url, addEntry) {
 
 	});
 }
+
+TweenMax.from(".bg__stars", .45, {y:550});
+TweenMax.staggerFrom(".bg__stars", 70, {rotation: -360, delay: .47});
+TweenMax.from(".screen__title-text", .45, {y:550, delay:.07});
+TweenMax.from(".bg__card-middle ", .45, {y:550, delay:.09});
+TweenMax.from(".bg__card-right ", .45, {y:550, delay:.09});
