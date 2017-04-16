f0d3ebe29f436d9be9fc8fb9f3922aecfb5ef529
d.m.luzhin <d.m.luzhin@gmail.com>
2017-04-16 17:33:02 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 //= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/gsap/src/minified/TweenLite.min.js
+//= ../../bower_components/gsap/src/minified/TweenMax.min.js
 //= partials/app.js
